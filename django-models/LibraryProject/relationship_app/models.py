e10990f4faea746d134cf1a41cc3e68e701ff756
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-07-31 22:44:05 +0200

    Django views and URL config fixes

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,8 +9,9 @@ class Author(models.Model):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField(null=True, blank=True)  
 
     def __str__(self):
         return self.title
