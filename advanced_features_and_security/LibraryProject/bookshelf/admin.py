2bc1f7d37be79345f63c5d9212ff5aafeb74cd9c
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-08-02 23:22:34 +0200

    Implemented custom user model and registered it in admin; added Book admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from .models import Book
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')   # Show these fields in list view
-    search_fields = ('title', 'author')                      # Add search box for title & author
-    list_filter = ('publication_year',)                      # Add filter sidebar for publication year
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+admin.site.register(CustomUser, CustomUserAdmin)
