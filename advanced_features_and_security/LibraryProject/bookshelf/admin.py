de4a9573cfe216ba121d98b50b76e49e93f827de
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-08-03 00:16:11 +0200

    Fixed: CustomUser model, admin registration, and authentication updates

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Book, CustomUser
+from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -8,16 +7,3 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
     list_filter = ('publication_year',)
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
-    search_fields = ('username', 'email')
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
-
-admin.site.register(CustomUser, CustomUserAdmin)
