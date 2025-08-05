eeb38254420a563a9a7cad4ae6ad89461d277f73
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-08-05 17:22:55 +0200

    Fixed circular imports: moved BookList to views.py, cleaned up models.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
-from .models import BookList 
+from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
-    queryset = BookList.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
