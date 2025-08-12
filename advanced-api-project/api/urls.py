69bd9ab47d9ea4f4a020d4c5599d6802b4234c61
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-08-13 00:45:16 +0200

    fixed

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,16 +1,15 @@
-from django.urls import path
-from .views import (
-    BookListView,
-    BookDetailView,
-    BookCreateView,
-    BookUpdateView,
-    BookDeleteView,
-)
+from sys import path
+from django.views.generic.detail import BaseDetailView
+from django.views.generic.edit import BaseCreateView, BaseDeleteView, BaseUpdateView
+from django.views.generic.list import BaseListView
+
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/', BaseListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BaseDetailView.as_view(), name='book-detail'),
+    path('books/create/', BaseCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BaseUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BaseDeleteView.as_view(), name='book-delete'),
+    path('books/update/', BaseUpdateView.as_view(), name='book-update-no-pk'),
+    path('books/delete/', BaseDeleteView.as_view(), name='book-delete-no-pk'),
 ]
