6b3b825bee70294074b46addc50541dfb3805b4a
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-08-12 23:48:55 +0200

    Add token authentication and permissions to BookViewSet

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from your_app.views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
