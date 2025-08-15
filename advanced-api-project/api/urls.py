37d6c87b3d73fd89cee6e7e07d84f711ce6430ff
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-08-15 11:25:05 +0200

    Fixing urls.py to pass unit tests.

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,15 +1,11 @@
-from sys import path
-from django.views.generic.detail import BaseDetailView
-from django.views.generic.edit import BaseCreateView, BaseDeleteView, BaseUpdateView
-from django.views.generic.list import BaseListView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, AuthorViewSet
 
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
+router.register(r'authors', AuthorViewSet, basename='author')
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BaseDetailView.as_view(), name='book-detail'),
-    path('books/create/', BaseCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BaseUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BaseDeleteView.as_view(), name='book-delete'),
-    path('books/update/', BaseUpdateView.as_view(), name='book-update-no-pk'),
-    path('books/delete/', BaseDeleteView.as_view(), name='book-delete-no-pk'),
+    path('', include(router.urls)),
 ]
