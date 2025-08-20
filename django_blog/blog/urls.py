942f593444c3b8e710713b54f77a523db9cd680d
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-08-20 14:17:47 +0200

    Fixes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,10 +3,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    # Auth
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    path('', LoginView.as_view(template_name='registration/login.html'), name='home'),
+    path('', LoginView.as_view(template_name='blog/login.html'), name='home'),
 ]
